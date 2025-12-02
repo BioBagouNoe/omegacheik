6dda78fd1b2d7e3a8b6fd49fa4122f6a087273ab
Adjaho Sara <adjahosara2003@gmail.com>
2025-12-02 23:06:08 +0100

    EXPORTATION LIGNE

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,14 @@ Route::get('/manifest-details', function () {
 Route::get('/manifests', function () {
     return view('manifest.index');
 })->name('manifests');
+// Route pour l'import/export Excel
+Route::post('lines/import', [App\Http\Controllers\LineController::class, 'import'])->name('lines.import');
+Route::get('lines/export', [App\Http\Controllers\LineController::class, 'export'])->name('lines.export');
+
 // Routes CRUD pour les lignes
 Route::resource('lines', App\Http\Controllers\LineController::class);
 // Routes CRUD pour les agences
 Route::resource('agencies', App\Http\Controllers\AgencyController::class);
-
-// Route pour l'import/export Excel
-Route::post('lines/import', [App\Http\Controllers\LineController::class, 'import'])->name('lines.import');
-Route::get('lines/export', [App\Http\Controllers\LineController::class, 'export'])->name('lines.export');
 Route::get('/ships', function () {
     return view('ship.index');
 })->name('ships');
